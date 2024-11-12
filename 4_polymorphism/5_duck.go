a502d8c632494bdc84b74bd17cbde3e538d8330a
Haonian Fan <fhn037@126.com>
2024-11-12 21:37:17 +0800

    add value and ptr receiver of golang

--- 4_polymorphism/5_duck.go
+++ 4_polymorphism/5_duck.go
@@ -4,27 +4,60 @@ import "fmt"
 
 // 定义接口
 type Sensor interface {
-    ReadValue() float64
-    GetStatus() string
+	ReadValue() float64
+	GetStatus() string
 }
 
 type TemperatureSensor struct {
+	temperature float64
 }
 
+// 使用指针类型接收器
+// func (t *TemperatureSensor) ReadValue() float64 {
+// 	return t.temperature
+// }
+
+// func (t *TemperatureSensor) GetStatus() string {
+// 	return "Temperature Sensor OK"
+// }
+
 func (t TemperatureSensor) ReadValue() float64 {
-    return 25.0
+	return t.temperature
 }
 
 func (t TemperatureSensor) GetStatus() string {
-    return "Temperature Sensor OK"
+	return "Temperature Sensor OK"
+}
+
+func (t *TemperatureSensor) Update(newTemp float64) {
+	t.temperature = newTemp
 }
 
 func PrintSensorStatus(s Sensor) {
-    fmt.Printf("Value: %.2f, Status: %s\n", s.ReadValue(), s.GetStatus())
+	fmt.Printf("Value: %.2f, Status: %s\n", s.ReadValue(), s.GetStatus())
 }
 
 func main() {
-    var tempSensor Sensor
-    tempSensor = TemperatureSensor{}
-    PrintSensorStatus(tempSensor)
+	var tempSensor Sensor
+	// 使用指针类型接收器
+	// tempSensor = &TemperatureSensor{
+	// 	temperature: 25,
+	// }
+	// PrintSensorStatus(tempSensor)
+	// // 使用类型断言
+	// if ts, ok := tempSensor.(*TemperatureSensor); ok {
+	// 	ts.Update(26)
+	// }
+	// PrintSensorStatus(tempSensor)
+
+	// 使用值类型接收器
+	tempSensor = TemperatureSensor{
+		temperature: 25,
+	}
+	PrintSensorStatus(tempSensor)
+	// 使用类型断言
+	if ts, ok := tempSensor.(TemperatureSensor); ok {
+		ts.Update(26)
+	}
+	PrintSensorStatus(tempSensor)
 }
